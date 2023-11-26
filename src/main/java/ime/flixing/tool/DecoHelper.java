af088701aaebea5aba6d9caf287509e57d733228
IvanM <ivan.montes@outlook.com>
2023-11-26 09:47:18 +0100

    Add more messages

--- src/main/java/ime/flixing/tool/DecoHelper.java
+++ src/main/java/ime/flixing/tool/DecoHelper.java
@@ -23,10 +23,12 @@ public class DecoHelper {
 		
 	public static final String MSG_VOLVER = "Enter option number (0 for back): ";
 	public static final String MSG_SALIR = "Enter option number (0 for exit): ";
-	public static final String MSG_NULL_ERROR = "Null object found";
-	public static final String MSG_OPTERROR = "Wrong option";
-	public static final String MSG_COD_ERROR = "Wrong code";
-	public static final String MSG_DATA_ERROR = "Some data is Wrong";
+	public static final String MSG_ERROR_NULL = "Null object found";
+	public static final String MSG_ERROR_OPT = "Wrong option";
+	public static final String MSG_ERROR_COD = "Wrong code";
+	public static final String MSG_ERROR_DATA = "Some data is Wrong";
+	public static final String MSG_ERROR_PROCESS = "Unable to terminate the process";
+	public static final String MSG_ERROR_UNEXPECTED = "Unexpected error";
 	public static final String MSG_WRITE_COD = "Next, Write a valid code ";
 	public static final String MSG_WRITE_COD_FLIX = "Please, Write a valid FLIX code ";
 	public static final String MSG_WRITE_COD_PERSON = "Next, Write a valid PERSON code ";
@@ -43,5 +45,7 @@ public class DecoHelper {
 	public static final String MSG_ERROR_DELETE_ASSOCIATED_ITEMS = "Some items are still associated to this entity. Impossible to delete. Please, first empty the list";
 	public static final String MSG_REGISTRY_REPEATED = "Repeated info: This registry is already in the database";
 	public static final String MSG_DUPLICATED_NAME = "Repeated info: This NAME is already in the database";
+	public static final String MSG_CONFIRM_OPTION = "Please, confirm option";
+	public static final String MSG_CONFIRM_TITLE = "Confirmation box";
 
 }
