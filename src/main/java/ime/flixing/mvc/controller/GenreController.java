7e917a0c3c838e4d77150d2383c7e54660230aab
IvanM <ivan.montes@outlook.com>
2023-11-27 17:18:11 +0100

    Change class attributes to variables

--- src/main/java/ime/flixing/mvc/controller/GenreController.java
+++ src/main/java/ime/flixing/mvc/controller/GenreController.java
@@ -1,22 +1,51 @@
 package ime.flixing.mvc.controller;
 
 import ime.flixing.mvc.view.GenreView;
+import ime.flixing.mvc.view.genre.*;
 
 public class GenreController {
-
-	private static GenreView genreView;
 	
 	public static final void initGenreController() {
 		
-		genreView = new GenreView();
+		GenreView genreView = new GenreView();
 		genreView.setVisible(true);
 		
 	}
 	
-	public static final void closeGenreView() {
+	public static final void initGenreDeleteView() {
 		
-		genreView.setVisible(false);
-		genreView = null;
+		GenreDeleteView genreDeleteView = new GenreDeleteView();
+		genreDeleteView.setVisible(true);
 		
 	}
+	
+	public static final void initGenreGetAllView() {
+		
+		GenreGetAllView genreGetAllView = new GenreGetAllView();
+		genreGetAllView.setVisible(true);
+		
+	}
+
+	public static final void initGenreGetByIdView() {
+		
+		GenreGetByIdView genreGetByIdView = new GenreGetByIdView();
+		genreGetByIdView.setVisible(true);
+		
+	}
+
+	public static final void initGenreSaveView() {
+		
+		GenreSaveView genreSaveView = new GenreSaveView();
+		genreSaveView.setVisible(true);
+		
+	}
+
+	public static final void initGenreUpdateView() {
+		
+		GenreUpdateView genreUpdateView = new GenreUpdateView();
+		genreUpdateView.setVisible(true);
+		
+	}
+	
+	
 }
