c650305cc53dacc4dd05c92102aadd3443657628
IvanM <ivan.montes@outlook.com>
2023-11-28 10:28:01 +0100

    Minor changes

--- src/main/java/ime/flixing/mvc/view/GenreView.java
+++ src/main/java/ime/flixing/mvc/view/GenreView.java
@@ -47,38 +47,38 @@ public class GenreView extends JDialog {
 		{
 			lblGenreTitle = new JLabel("GENRE");
 			sl_contentPanel.putConstraint(SpringLayout.NORTH, lblGenreTitle, 10, SpringLayout.NORTH, contentPanel);
-			sl_contentPanel.putConstraint(SpringLayout.WEST, lblGenreTitle, 176, SpringLayout.WEST, contentPanel);
 			lblGenreTitle.setFont(new Font("Tahoma", Font.BOLD, 16));
 			contentPanel.add(lblGenreTitle);
 		}
 		
 		JButton btnGetAll = new JButton("Get All");
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnGetAll, 0, SpringLayout.WEST, lblGenreTitle);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, lblGenreTitle, 0, SpringLayout.WEST, btnGetAll);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, btnGetAll, 163, SpringLayout.WEST, contentPanel);
 		btnGetAll.addActionListener( e -> GenreController.initGenreGetAllView());
 		contentPanel.add(btnGetAll);
 		
 		JButton btnGetById = new JButton("Get By Id");
 		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnGetAll, -6, SpringLayout.NORTH, btnGetById);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnGetById, 0, SpringLayout.WEST, lblGenreTitle);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, btnGetById, 0, SpringLayout.WEST, btnGetAll);
 		btnGetById.addActionListener( e -> GenreController.initGenreGetByIdView());
 		contentPanel.add(btnGetById);
 		
 		JButton btnSave = new JButton("Save");
 		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnGetById, -6, SpringLayout.NORTH, btnSave);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnSave, 0, SpringLayout.WEST, lblGenreTitle);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, btnSave, 0, SpringLayout.WEST, btnGetAll);
 		btnSave.addActionListener( e -> GenreController.initGenreSaveView());
 		contentPanel.add(btnSave);
 		
 		JButton btnUpdate = new JButton("Update");
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnUpdate, -91, SpringLayout.SOUTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnSave, -6, SpringLayout.NORTH, btnUpdate);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnUpdate, 0, SpringLayout.WEST, lblGenreTitle);
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnUpdate, -90, SpringLayout.SOUTH, contentPanel);
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnSave, -7, SpringLayout.NORTH, btnUpdate);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, btnUpdate, 0, SpringLayout.WEST, btnGetAll);
 		btnUpdate.addActionListener( e -> GenreController.initGenreUpdateView());
 		contentPanel.add(btnUpdate);
 		
 		JButton btnDelete = new JButton("Delete");
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnDelete, 7, SpringLayout.SOUTH, btnUpdate);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnDelete, 0, SpringLayout.WEST, lblGenreTitle);
+		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnDelete, 6, SpringLayout.SOUTH, btnUpdate);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, btnDelete, 0, SpringLayout.WEST, btnGetAll);
 		btnDelete.addActionListener( e -> GenreController.initGenreDeleteView());
 		contentPanel.add(btnDelete);
 		
