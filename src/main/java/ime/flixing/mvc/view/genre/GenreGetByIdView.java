85719556e830aa8a243b3ffde0ccfc01df769ad3
IvanM <ivan.montes@outlook.com>
2023-11-28 10:25:57 +0100

    Complete searchAndShowGenre method

--- src/main/java/ime/flixing/mvc/view/genre/GenreGetByIdView.java
+++ src/main/java/ime/flixing/mvc/view/genre/GenreGetByIdView.java
@@ -2,6 +2,7 @@ package ime.flixing.mvc.view.genre;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.util.Optional;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -9,6 +10,8 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import ime.flixing.entity.Genre;
+import ime.flixing.mvc.controller.GenreController;
 import ime.flixing.tool.Checker;
 import ime.flixing.tool.DecoHelper;
 
@@ -20,9 +23,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 
-import java.awt.Dimension;
-import javax.swing.JTextPane;
 import javax.swing.JTextArea;
+import javax.swing.JSeparator;
 
 public class GenreGetByIdView extends JDialog {
 
@@ -78,25 +80,50 @@ public class GenreGetByIdView extends JDialog {
 		jListGenre.setModel(new DefaultListModel<String>());
 		
 		JScrollPane spListGenre = new JScrollPane();
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, spListGenre, 24, SpringLayout.SOUTH, spGenreCod);
+		sl_contentPanel.putConstraint(SpringLayout.NORTH, spListGenre, 36, SpringLayout.SOUTH, spGenreCod);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, spListGenre, 0, SpringLayout.WEST, lblGenreCod);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, spListGenre, -5, SpringLayout.SOUTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, spListGenre, 36, SpringLayout.EAST, btnSearch);
 		spListGenre.setViewportView(jListGenre);
 		contentPanel.add(spListGenre);
 		
 		JScrollPane spDescription = new JScrollPane();
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, spDescription, 54, SpringLayout.NORTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, spDescription, 33, SpringLayout.EAST, spListGenre);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, spDescription, 0, SpringLayout.SOUTH, spListGenre);
+		sl_contentPanel.putConstraint(SpringLayout.EAST, spListGenre, 0, SpringLayout.EAST, spDescription);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, spDescription, 0, SpringLayout.WEST, lblGenreCod);
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, spDescription, -5, SpringLayout.SOUTH, contentPanel);
 		sl_contentPanel.putConstraint(SpringLayout.EAST, spDescription, -15, SpringLayout.EAST, contentPanel);
 		contentPanel.add(spDescription);
 		
 		textAreaDescription = new JTextArea();
+		textAreaDescription.setWrapStyleWord(true);
+		textAreaDescription.setLineWrap(true);
 		textAreaDescription.setEditable(false);
 		spDescription.setViewportView(textAreaDescription);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, textAreaDescription, 46, SpringLayout.EAST, spListGenre);
 		sl_contentPanel.putConstraint(SpringLayout.SOUTH, textAreaDescription, -150, SpringLayout.SOUTH, contentPanel);
+		
+		JButton btnClean = new JButton("Clean");
+		sl_contentPanel.putConstraint(SpringLayout.NORTH, spDescription, 121, SpringLayout.SOUTH, btnClean);
+		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnClean, 0, SpringLayout.NORTH, btnSearch);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, btnClean, 24, SpringLayout.EAST, btnSearch);
+		btnClean.addActionListener( e -> cleanFields() );
+		contentPanel.add(btnClean);
+		
+		JLabel lblDescription = new JLabel("Description");
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, spListGenre, -8, SpringLayout.NORTH, lblDescription);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, lblDescription, 0, SpringLayout.WEST, lblGenreCod);
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, lblDescription, -6, SpringLayout.NORTH, spDescription);
+		contentPanel.add(lblDescription);
+		
+		JLabel lblGenre = new JLabel("Genre");
+		sl_contentPanel.putConstraint(SpringLayout.WEST, lblGenre, 10, SpringLayout.WEST, contentPanel);
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, lblGenre, -6, SpringLayout.NORTH, spListGenre);
+		contentPanel.add(lblGenre);
+		
+		JSeparator separator = new JSeparator();
+		sl_contentPanel.putConstraint(SpringLayout.NORTH, separator, 6, SpringLayout.SOUTH, spGenreCod);
+		sl_contentPanel.putConstraint(SpringLayout.WEST, separator, 0, SpringLayout.WEST, lblGenreCod);
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, separator, -5, SpringLayout.NORTH, lblGenre);
+		sl_contentPanel.putConstraint(SpringLayout.EAST, separator, -10, SpringLayout.EAST, contentPanel);
+		contentPanel.add(separator);
 				
 		
 		{
@@ -116,13 +143,31 @@ public class GenreGetByIdView extends JDialog {
 		
 		if ( Checker.checkDigits(spGenreCod.getValue().toString()) ){
 			
+			Optional<Genre>optGenreFound = GenreController.getGenreById( spGenreCod.getValue().toString() );
 			
+			if ( optGenreFound.isPresent() ) {
+				
+				jListGenre.setListData(new String[] {
+													"Id: " + optGenreFound.get().getGenreId().toString() + 
+													 "  //  " +
+											 		"Name: " + optGenreFound.get().getName() 
+											 		} 
+										);
+				textAreaDescription.setText( optGenreFound.get().getDescription() );
+				
+			}else {
+				JOptionPane.showMessageDialog(this, "\t" + DecoHelper.MSG_ERROR_NULL);
+			}
 			
 		}else {
 			JOptionPane.showMessageDialog(this, "\t" + DecoHelper.MSG_ERROR_DATA);
 		}
 		
 	}
+	
+	private void cleanFields() {
 		
-		
+		textAreaDescription.setText("");
+		jListGenre.setListData(new String[] {});
+	}
 }
