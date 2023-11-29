3d7dffbaa1e9f28c383d7da77bc6ad628213cb6c
IvanM <ivan.montes@outlook.com>
2023-11-29 17:54:30 +0100

    Create view for person menu

---
+++ src/main/java/ime/flixing/mvc/view/PersonView.java
@@ -0,0 +1,55 @@
+package ime.flixing.mvc.view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+
+public class PersonView extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			PersonView dialog = new PersonView();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public PersonView() {
+		setModal(true);
+		setAlwaysOnTop(true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new SpringLayout());
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnBack = new JButton("Back");
+				btnBack.setActionCommand("Cancel");
+				btnBack.addActionListener( e -> dispose() );
+				buttonPane.add(btnBack);
+			}
+		}
+	}
+
+}
