cac6882a60f8ca780d3e46ee5b19bc4e7c6fe8d3
IvanM <ivan.montes@outlook.com>
2023-11-30 17:41:28 +0100

    Developing view for get all

--- src/main/java/ime/flixing/mvc/view/person/PersonGetAllView.java
+++ src/main/java/ime/flixing/mvc/view/person/PersonGetAllView.java
@@ -7,11 +7,22 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.JSeparator;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class PersonGetAllView extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JButton btnSearch;
+	private JSeparator separator;
+	private JTable tPerson;
+	private JScrollPane spPerson;
+	private JButton btnClean;
 
 	/**
 	 * Launch the application.
@@ -30,27 +41,61 @@ public class PersonGetAllView extends JDialog {
 	 * Create the dialog.
 	 */
 	public PersonGetAllView() {
+		setModal(true);
+		setAlwaysOnTop(true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		SpringLayout sl_contentPanel = new SpringLayout();
+		contentPanel.setLayout(sl_contentPanel);
+		{
+			btnSearch = new JButton("Search");
+			sl_contentPanel.putConstraint(SpringLayout.WEST, btnSearch, 86, SpringLayout.WEST, contentPanel);
+			contentPanel.add(btnSearch);
+		}
+		{
+			btnClean = new JButton("Clean");
+			sl_contentPanel.putConstraint(SpringLayout.NORTH, btnClean, 10, SpringLayout.NORTH, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.NORTH, btnSearch, 0, SpringLayout.NORTH, btnClean);
+			sl_contentPanel.putConstraint(SpringLayout.EAST, btnClean, -102, SpringLayout.EAST, contentPanel);
+			contentPanel.add(btnClean);
+		}
+		{
+			separator = new JSeparator();
+			sl_contentPanel.putConstraint(SpringLayout.NORTH, separator, 4, SpringLayout.SOUTH, btnSearch);
+			sl_contentPanel.putConstraint(SpringLayout.WEST, separator, 20, SpringLayout.WEST, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.SOUTH, separator, -170, SpringLayout.SOUTH, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.EAST, separator, 400, SpringLayout.WEST, contentPanel);
+			contentPanel.add(separator);
+		}
+		{
+			spPerson = new JScrollPane();
+			sl_contentPanel.putConstraint(SpringLayout.NORTH, spPerson, 2, SpringLayout.SOUTH, separator);
+			sl_contentPanel.putConstraint(SpringLayout.WEST, spPerson, 36, SpringLayout.WEST, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.SOUTH, spPerson, -10, SpringLayout.SOUTH, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.EAST, spPerson, -35, SpringLayout.EAST, contentPanel);
+			contentPanel.add(spPerson);
+			
+			tPerson = new JTable();
+			tPerson.setFillsViewportHeight(true);
+			spPerson.setViewportView(tPerson);
+			sl_contentPanel.putConstraint(SpringLayout.NORTH, tPerson, 114, SpringLayout.SOUTH, separator);
+			sl_contentPanel.putConstraint(SpringLayout.WEST, tPerson, 48, SpringLayout.WEST, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.SOUTH, tPerson, -10, SpringLayout.SOUTH, contentPanel);
+			sl_contentPanel.putConstraint(SpringLayout.EAST, tPerson, 68, SpringLayout.WEST, contentPanel);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnBack = new JButton("Back");
+				btnBack.setActionCommand("Cancel");
+				btnBack.addActionListener( e -> dispose() );
+				buttonPane.add(btnBack);
 			}
 		}
 	}
-
 }
