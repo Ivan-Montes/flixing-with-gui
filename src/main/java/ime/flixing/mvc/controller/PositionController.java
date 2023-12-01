9dffe2bda9c789011708da6be4b4da22b6983b5e
IvanM <ivan.montes@outlook.com>
2023-12-01 16:34:59 +0100

    Create controller for Position

---
+++ src/main/java/ime/flixing/mvc/controller/PositionController.java
@@ -0,0 +1,17 @@
+package ime.flixing.mvc.controller;
+
+import ime.flixing.mvc.view.PositionView;
+
+public class PositionController {
+
+	public static final void initPositionController() {
+
+		PositionView view = new PositionView();
+		view.setVisible(true);
+		
+	}
+	
+	
+	
+
+}
