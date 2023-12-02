cf7097a9f6dfae8cd83accd3f8b9244f0e5dcde5
IvanM <ivan.montes@outlook.com>
2023-12-02 12:34:19 +0100

    Configure listeners

--- src/main/java/ime/flixing/mvc/view/PositionView.java
+++ src/main/java/ime/flixing/mvc/view/PositionView.java
@@ -7,6 +7,9 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import ime.flixing.mvc.controller.PositionController;
+
 import javax.swing.SpringLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -43,35 +46,40 @@ public class PositionView extends JDialog {
 		SpringLayout sl_contentPanel = new SpringLayout();
 		contentPanel.setLayout(sl_contentPanel);
 		
-		JLabel lblPosition = new JLabel("Position");
+		JLabel lblPosition = new JLabel("POSITION");
 		lblPosition.setFont(new Font("Tahoma", Font.BOLD, 14));
 		sl_contentPanel.putConstraint(SpringLayout.NORTH, lblPosition, 10, SpringLayout.NORTH, contentPanel);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, lblPosition, 167, SpringLayout.WEST, contentPanel);
 		contentPanel.add(lblPosition);
 		
 		JButton btnGetAll = new JButton("Get All");
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnGetAll, 18, SpringLayout.SOUTH, lblPosition);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, btnGetAll, 0, SpringLayout.WEST, lblPosition);
+		btnGetAll.addActionListener( e -> PositionController.initPositionGetAllView() );
 		contentPanel.add(btnGetAll);
 		
 		JButton btnGetById = new JButton("Get By Id");
+		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnGetAll, -6, SpringLayout.NORTH, btnGetById);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, btnGetById, 0, SpringLayout.WEST, lblPosition);
+		btnGetById.addActionListener( e -> PositionController.initPositionGetByIdView() );
 		contentPanel.add(btnGetById);
 		
 		JButton btnSave = new JButton("Save");
 		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnGetById, -6, SpringLayout.NORTH, btnSave);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, btnSave, 0, SpringLayout.WEST, lblPosition);
+		btnSave.addActionListener( e -> PositionController.initPositionSaveView() );
 		contentPanel.add(btnSave);
 		
 		JButton btnUpdate = new JButton("Update");
 		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnUpdate, -63, SpringLayout.SOUTH, contentPanel);
 		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnSave, -6, SpringLayout.NORTH, btnUpdate);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, btnUpdate, 0, SpringLayout.WEST, lblPosition);
+		btnUpdate.addActionListener( e -> PositionController.initPositionUpdateView() );
 		contentPanel.add(btnUpdate);
 		
 		JButton btnDelete = new JButton("Delete");
 		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnDelete, 6, SpringLayout.SOUTH, btnUpdate);
 		sl_contentPanel.putConstraint(SpringLayout.WEST, btnDelete, 0, SpringLayout.WEST, lblPosition);
+		btnDelete.addActionListener( e -> PositionController.initPositionDeleteView() );
 		contentPanel.add(btnDelete);
 		{
 			JPanel buttonPane = new JPanel();
@@ -80,6 +88,7 @@ public class PositionView extends JDialog {
 			{
 				JButton btnBack = new JButton("Back");
 				btnBack.setActionCommand("Cancel");
+				btnBack.addActionListener( e -> dispose() );
 				buttonPane.add(btnBack);
 			}
 		}
