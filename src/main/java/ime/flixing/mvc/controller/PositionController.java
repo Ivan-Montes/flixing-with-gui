3ab80874a21c5ffb03189d89444049783ecdb3f2
IvanM <ivan.montes@outlook.com>
2023-12-02 12:31:08 +0100

    Add calls for each view

--- src/main/java/ime/flixing/mvc/controller/PositionController.java
+++ src/main/java/ime/flixing/mvc/controller/PositionController.java
@@ -1,6 +1,13 @@
 package ime.flixing.mvc.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import ime.flixing.dao.impl.PositionDaoImpl;
+import ime.flixing.entity.Position;
 import ime.flixing.mvc.view.PositionView;
+import ime.flixing.mvc.view.position.*;
+import ime.flixing.tool.Checker;
 
 public class PositionController {
 
@@ -10,8 +17,59 @@ public class PositionController {
 		view.setVisible(true);
 		
 	}
+
+	public static final void initPositionGetAllView() {
+
+		PositionGetAllView view = new PositionGetAllView();
+		view.setVisible(true);
+		
+	}
+
+	public static final void initPositionGetByIdView() {
+
+		PositionGetByIdView view = new PositionGetByIdView();
+		view.setVisible(true);
+		
+	}
+
+	public static final void initPositionSaveView() {
+
+		PositionSaveView view = new PositionSaveView();
+		view.setVisible(true);
+		
+	}
+
+	public static final void initPositionUpdateView() {
+
+		PositionUpdateView view = new PositionUpdateView();
+		view.setVisible(true);
+		
+	}
+
+	public static final void initPositionDeleteView() {
+
+		PositionDeleteView view = new PositionDeleteView();
+		view.setVisible(true);
+		
+	}
 	
+	public static final Optional<List<Position>>getAllPosition(){
+		
+		return Optional.ofNullable( new PositionDaoImpl().getAllPosition() );
+		
+	}
 	
-	
+	public static final Optional<Position>getPositionById(String strPositionCod){
+		
+		Optional<Position>optPosition  = Optional.empty();
+		
+		if ( Checker.checkDigits(strPositionCod) ) {
+			
+			optPosition = Optional.ofNullable( new PositionDaoImpl().getPositionById(Long.parseLong(strPositionCod)));
+		}
+		
+		return optPosition;
+		
+	}
 
 }
