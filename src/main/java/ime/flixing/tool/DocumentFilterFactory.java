0865625d2c28b15d00f113d02046a4600d7c8cc2
IvanM <ivan.montes@outlook.com>
2023-12-03 10:10:56 +0100

    Developing Factory for DocumentFilter objects

---
+++ src/main/java/ime/flixing/tool/DocumentFilterFactory.java
@@ -0,0 +1,38 @@
+package ime.flixing.tool;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor( access= AccessLevel.PRIVATE )
+public class DocumentFilterFactory {
+
+	public static final DocumentFilter createDocumentFilter(int type) {
+		
+        String regex = (type == 1) ? CheckerPattern.CHECKERPATTERN_NAME_BASIC : CheckerPattern.CHECKERPATTERN_DESCRIPTION_BASIC;
+        
+        return new DocumentFilter() {
+            @Override
+            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+                String newStr = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
+                if (newStr.matches(regex)) {
+                    super.insertString(fb, offset, string, attr);
+                }
+            }
+
+            @Override
+            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                String newStr = fb.getDocument().getText(0, fb.getDocument().getLength()) + text;
+                if (newStr.matches(regex)) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+        };
+    }
+	
+	
+	
+}
