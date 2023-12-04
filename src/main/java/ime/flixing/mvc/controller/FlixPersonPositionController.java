eb1f6377de1bcd7ec7e1104e913f49b5eea1235d
IvanM <ivan.montes@outlook.com>
2023-12-04 19:05:12 +0100

    Add controller for FlixPersonPosition class

---
+++ src/main/java/ime/flixing/mvc/controller/FlixPersonPositionController.java
@@ -0,0 +1,17 @@
+package ime.flixing.mvc.controller;
+
+import ime.flixing.mvc.view.FlixPersonPositionView;
+
+
+public class FlixPersonPositionController {
+	
+	public static final void initFlixPersonPositionController() {
+		
+		FlixPersonPositionView view = new FlixPersonPositionView();
+		view.setVisible(true);
+	}
+	
+	
+	
+
+}
