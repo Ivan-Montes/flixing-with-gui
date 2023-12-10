f6ba79bcd2cb605aba2621cdbfc3a5ca347de1f8
IvanM <ivan.montes@outlook.com>
2023-12-10 16:28:15 +0100

    Replace showResultInfo as local method

--- src/main/java/ime/flixing/mvc/view/position/PositionSaveView.java
+++ src/main/java/ime/flixing/mvc/view/position/PositionSaveView.java
@@ -1,27 +1,20 @@
 package ime.flixing.mvc.view.position;
 
 import java.awt.BorderLayout;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.awt.FlowLayout;
-import java.text.ParseException;
 import java.util.Optional;
 
-import javax.swing.InputVerifier;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.AbstractDocument;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.DocumentFilter;
-import javax.swing.text.MaskFormatter;
-
-import ime.flixing.entity.Genre;
 import ime.flixing.entity.Position;
-import ime.flixing.mvc.controller.GenreController;
 import ime.flixing.mvc.controller.PositionController;
 import ime.flixing.tool.Checker;
+import ime.flixing.tool.CheckerPattern;
 import ime.flixing.tool.DecoHelper;
 import ime.flixing.tool.DocumentFilterFactory;
 
@@ -46,10 +39,11 @@ public class PositionSaveView extends JDialog {
 	public static void main(String[] args) {
 		try {
 			PositionSaveView dialog = new PositionSaveView();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 			dialog.setVisible(true);
 		} catch (Exception e) {
-			e.printStackTrace();
+			final Logger logger = Logger.getLogger(PositionSaveView.class.getName());
+			logger.log(Level.SEVERE, DecoHelper.MSG_SHIT_HAPPENS, e);
 		}
 	}
 
@@ -58,75 +52,60 @@ public class PositionSaveView extends JDialog {
 	 */
 	public PositionSaveView() {
 		setModal(true);
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 		setAlwaysOnTop(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		SpringLayout sl_contentPanel = new SpringLayout();
-		contentPanel.setLayout(sl_contentPanel);
+		SpringLayout slContentPanel = new SpringLayout();
+		contentPanel.setLayout(slContentPanel);
 		
 		JLabel lblName = new JLabel("Position Name");
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, lblName, 10, SpringLayout.NORTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, lblName, 10, SpringLayout.WEST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, lblName, 10, SpringLayout.NORTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.WEST, lblName, 10, SpringLayout.WEST, contentPanel);
 		contentPanel.add(lblName);
 		
 		JLabel lblDescription = new JLabel("Description");
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, lblDescription, 48, SpringLayout.NORTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, lblDescription, 169, SpringLayout.WEST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, lblDescription, 48, SpringLayout.NORTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.WEST, lblDescription, 169, SpringLayout.WEST, contentPanel);
 		contentPanel.add(lblDescription);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, scrollPane, 6, SpringLayout.SOUTH, lblDescription);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, scrollPane, -392, SpringLayout.EAST, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, scrollPane, -58, SpringLayout.SOUTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, scrollPane, -31, SpringLayout.EAST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, scrollPane, 6, SpringLayout.SOUTH, lblDescription);
+		slContentPanel.putConstraint(SpringLayout.WEST, scrollPane, -392, SpringLayout.EAST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.SOUTH, scrollPane, -58, SpringLayout.SOUTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.EAST, scrollPane, -31, SpringLayout.EAST, contentPanel);
 		contentPanel.add(scrollPane);
 		
 		taDescription = new JTextArea();
 		taDescription.setWrapStyleWord(true);
 		
 		taDescription.setLineWrap(true);
-		((AbstractDocument)taDescription.getDocument()).setDocumentFilter(DocumentFilterFactory.createDocumentFilter(1));
+		((AbstractDocument)taDescription.getDocument()).setDocumentFilter(
+				DocumentFilterFactory.createDocumentFilter(CheckerPattern.DESCRIPTION_FULL));
 		
 		scrollPane.setViewportView(taDescription);
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, taDescription, -101, SpringLayout.SOUTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, taDescription, -134, SpringLayout.EAST, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, taDescription, -79, SpringLayout.SOUTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, taDescription, -119, SpringLayout.EAST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, taDescription, -101, SpringLayout.SOUTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.WEST, taDescription, -134, SpringLayout.EAST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.SOUTH, taDescription, -79, SpringLayout.SOUTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.EAST, taDescription, -119, SpringLayout.EAST, contentPanel);
 		
 		JButton btnSave = new JButton("Save");
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnSave, 13, SpringLayout.SOUTH, scrollPane);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnSave, 165, SpringLayout.WEST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, btnSave, 13, SpringLayout.SOUTH, scrollPane);
+		slContentPanel.putConstraint(SpringLayout.WEST, btnSave, 165, SpringLayout.WEST, contentPanel);
 		btnSave.addActionListener( e -> savePosition() );
 		contentPanel.add(btnSave);
 	
 		
 		ftfPositionName = new JFormattedTextField();
-	
-	       ((AbstractDocument) ftfPositionName.getDocument()).setDocumentFilter(new DocumentFilter() {
-	            @Override
-	            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
-	                String newStr = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
-	                if (newStr.matches("[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+")) {
-	                    super.insertString(fb, offset, string, attr);
-	                }
-	            }
+		((AbstractDocument)ftfPositionName.getDocument()).setDocumentFilter(
+				DocumentFilterFactory.createDocumentFilter(CheckerPattern.NAME_FULL));
 
-	            @Override
-	            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-	                String newStr = fb.getDocument().getText(0, fb.getDocument().getLength()) + text;
-	                if (newStr.matches("[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+")) {
-	                    super.replace(fb, offset, length, text, attrs);
-	                }
-	            }
-	        });
         
-        
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, ftfPositionName, -3, SpringLayout.NORTH, lblName);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, ftfPositionName, 11, SpringLayout.EAST, lblName);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, ftfPositionName, 0, SpringLayout.EAST, scrollPane);
+		slContentPanel.putConstraint(SpringLayout.NORTH, ftfPositionName, -3, SpringLayout.NORTH, lblName);
+		slContentPanel.putConstraint(SpringLayout.WEST, ftfPositionName, 11, SpringLayout.EAST, lblName);
+		slContentPanel.putConstraint(SpringLayout.EAST, ftfPositionName, 0, SpringLayout.EAST, scrollPane);
 		contentPanel.add(ftfPositionName);
 		{
 			JPanel buttonPane = new JPanel();
@@ -156,7 +135,8 @@ public class PositionSaveView extends JDialog {
 				
 				if (optPos.isPresent() ) {
 					
-					showResultInfo(optPos);
+					JOptionPane.showMessageDialog(this, 
+							DecoHelper.chooseResultMessageByReturnValue( optPos.get().getPositionId() ) );
 					cleanFields();
 					
 				}else {
@@ -167,33 +147,6 @@ public class PositionSaveView extends JDialog {
 		}else {
 			JOptionPane.showMessageDialog(this, DecoHelper.MSG_ERROR_DATA);
 		}
-		
-	}
-	
-	private void showResultInfo(Optional<Position> optPos) {
-		
-		String msgInfo = DecoHelper.MSG_ERROR_UNEXPECTED;
-		
-		if (optPos.isPresent() && optPos.get().getPositionId() != null ) {
-			
-			switch( String.valueOf(optPos.get().getPositionId() ) ) {
-			
-			case "-2":
-				msgInfo = DecoHelper.MSG_ERROR_CHECKER;
-				break;
-			case "-1":
-				msgInfo = DecoHelper.MSG_DUPLICATED_NAME;
-				break;
-			case "0":
-				msgInfo = DecoHelper.MSG_ERROR_UNEXPECTED;
-				break;
-			default:
-				msgInfo = DecoHelper.MSG_SUCCESSFULLY;
-				break;
-			}
-		}
-		
-		JOptionPane.showMessageDialog(this, msgInfo);
 		
 	}
 
