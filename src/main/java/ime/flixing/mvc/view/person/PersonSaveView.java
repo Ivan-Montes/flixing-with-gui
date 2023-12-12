6246007980f4c3dd28d17b8d617db3e0a0c13dde
IvanM <ivan.montes@outlook.com>
2023-12-12 12:00:44 +0100

    Add cleanAllFields in view

--- src/main/java/ime/flixing/mvc/view/person/PersonSaveView.java
+++ src/main/java/ime/flixing/mvc/view/person/PersonSaveView.java
@@ -1,6 +1,8 @@
 package ime.flixing.mvc.view.person;
 
 import java.awt.BorderLayout;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.awt.FlowLayout;
 import java.util.Optional;
 
@@ -31,17 +33,19 @@ public class PersonSaveView extends JDialog {
 	private JLabel lblPersonSurname;
 	private JButton btnClean;
 	private JSeparator separator;
-
+	private JTextPane tpResult;
+	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
 			PersonSaveView dialog = new PersonSaveView();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 			dialog.setVisible(true);
 		} catch (Exception e) {
-			e.printStackTrace();
+			final Logger logger = Logger.getLogger(PersonSaveView.class.getName());
+			logger.log(Level.SEVERE, DecoHelper.MSG_SHIT_HAPPENS, e);
 		}
 	}
 
@@ -49,69 +53,70 @@ public class PersonSaveView extends JDialog {
 	 * Create the dialog.
 	 */
 	public PersonSaveView() {
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 		setModal(true);
 		setAlwaysOnTop(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		SpringLayout sl_contentPanel = new SpringLayout();
-		contentPanel.setLayout(sl_contentPanel);
+		SpringLayout slContentPanel = new SpringLayout();
+		contentPanel.setLayout(slContentPanel);
 		{
 			lblPersonName = new JLabel("Person Name");
-			sl_contentPanel.putConstraint(SpringLayout.NORTH, lblPersonName, 10, SpringLayout.NORTH, contentPanel);
-			sl_contentPanel.putConstraint(SpringLayout.WEST, lblPersonName, 20, SpringLayout.WEST, contentPanel);
+			slContentPanel.putConstraint(SpringLayout.NORTH, lblPersonName, 10, SpringLayout.NORTH, contentPanel);
+			slContentPanel.putConstraint(SpringLayout.WEST, lblPersonName, 20, SpringLayout.WEST, contentPanel);
 			contentPanel.add(lblPersonName);
 		}
 		{
 			lblPersonSurname = new JLabel("Person Surname");
-			sl_contentPanel.putConstraint(SpringLayout.NORTH, lblPersonSurname, 25, SpringLayout.SOUTH, lblPersonName);
-			sl_contentPanel.putConstraint(SpringLayout.WEST, lblPersonSurname, 0, SpringLayout.WEST, lblPersonName);
+			slContentPanel.putConstraint(SpringLayout.NORTH, lblPersonSurname, 25, SpringLayout.SOUTH, lblPersonName);
+			slContentPanel.putConstraint(SpringLayout.WEST, lblPersonSurname, 0, SpringLayout.WEST, lblPersonName);
 			contentPanel.add(lblPersonSurname);
 		}
 		{
 			tfPersonName = new JTextField();
-			sl_contentPanel.putConstraint(SpringLayout.NORTH, tfPersonName, -3, SpringLayout.NORTH, lblPersonName);
-			sl_contentPanel.putConstraint(SpringLayout.WEST, tfPersonName, 27, SpringLayout.EAST, lblPersonName);
-			sl_contentPanel.putConstraint(SpringLayout.EAST, tfPersonName, -32, SpringLayout.EAST, contentPanel);
+			slContentPanel.putConstraint(SpringLayout.NORTH, tfPersonName, -3, SpringLayout.NORTH, lblPersonName);
+			slContentPanel.putConstraint(SpringLayout.WEST, tfPersonName, 27, SpringLayout.EAST, lblPersonName);
+			slContentPanel.putConstraint(SpringLayout.EAST, tfPersonName, -32, SpringLayout.EAST, contentPanel);
 			contentPanel.add(tfPersonName);
 			tfPersonName.setColumns(10);
 		}
 		{
 			tfPersonSurname = new JTextField();
-			sl_contentPanel.putConstraint(SpringLayout.NORTH, tfPersonSurname, -3, SpringLayout.NORTH, lblPersonSurname);
-			sl_contentPanel.putConstraint(SpringLayout.WEST, tfPersonSurname, 12, SpringLayout.EAST, lblPersonSurname);
-			sl_contentPanel.putConstraint(SpringLayout.EAST, tfPersonSurname, 0, SpringLayout.EAST, tfPersonName);
+			slContentPanel.putConstraint(SpringLayout.NORTH, tfPersonSurname, -3, SpringLayout.NORTH, lblPersonSurname);
+			slContentPanel.putConstraint(SpringLayout.WEST, tfPersonSurname, 12, SpringLayout.EAST, lblPersonSurname);
+			slContentPanel.putConstraint(SpringLayout.EAST, tfPersonSurname, 0, SpringLayout.EAST, tfPersonName);
 			contentPanel.add(tfPersonSurname);
 			tfPersonSurname.setColumns(10);
 		}
 		{
 			btnClean = new JButton("Clean");
-			sl_contentPanel.putConstraint(SpringLayout.WEST, btnClean, 170, SpringLayout.WEST, contentPanel);
-			sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnClean, -10, SpringLayout.SOUTH, contentPanel);
-			btnClean.addActionListener( e -> cleanFields() );
+			slContentPanel.putConstraint(SpringLayout.WEST, btnClean, 170, SpringLayout.WEST, contentPanel);
+			slContentPanel.putConstraint(SpringLayout.SOUTH, btnClean, -10, SpringLayout.SOUTH, contentPanel);
+			btnClean.addActionListener( e -> cleanAllFields() );
 			contentPanel.add(btnClean);
 		}
 		{
 			separator = new JSeparator();
-			sl_contentPanel.putConstraint(SpringLayout.NORTH, separator, 17, SpringLayout.SOUTH, tfPersonSurname);
-			sl_contentPanel.putConstraint(SpringLayout.WEST, separator, 20, SpringLayout.WEST, contentPanel);
-			sl_contentPanel.putConstraint(SpringLayout.SOUTH, separator, 31, SpringLayout.SOUTH, tfPersonSurname);
-			sl_contentPanel.putConstraint(SpringLayout.EAST, separator, 392, SpringLayout.WEST, contentPanel);
+			slContentPanel.putConstraint(SpringLayout.NORTH, separator, 17, SpringLayout.SOUTH, tfPersonSurname);
+			slContentPanel.putConstraint(SpringLayout.WEST, separator, 20, SpringLayout.WEST, contentPanel);
+			slContentPanel.putConstraint(SpringLayout.SOUTH, separator, 31, SpringLayout.SOUTH, tfPersonSurname);
+			slContentPanel.putConstraint(SpringLayout.EAST, separator, 392, SpringLayout.WEST, contentPanel);
 			contentPanel.add(separator);
 		}
 		
-		JTextPane tpResult = new JTextPane();
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, tpResult, 24, SpringLayout.SOUTH, separator);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, tpResult, 0, SpringLayout.WEST, lblPersonName);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, tpResult, -19, SpringLayout.NORTH, btnClean);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, tpResult, -32, SpringLayout.EAST, contentPanel);
+		tpResult = new JTextPane();
+		tpResult.setEditable(false);
+		slContentPanel.putConstraint(SpringLayout.NORTH, tpResult, 24, SpringLayout.SOUTH, separator);
+		slContentPanel.putConstraint(SpringLayout.WEST, tpResult, 0, SpringLayout.WEST, lblPersonName);
+		slContentPanel.putConstraint(SpringLayout.SOUTH, tpResult, -19, SpringLayout.NORTH, btnClean);
+		slContentPanel.putConstraint(SpringLayout.EAST, tpResult, -32, SpringLayout.EAST, contentPanel);
 		contentPanel.add(tpResult);
 		
 		JLabel lblResult = new JLabel("Result");
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, lblResult, 6, SpringLayout.SOUTH, separator);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, lblResult, 184, SpringLayout.WEST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, lblResult, 6, SpringLayout.SOUTH, separator);
+		slContentPanel.putConstraint(SpringLayout.WEST, lblResult, 184, SpringLayout.WEST, contentPanel);
 		contentPanel.add(lblResult);
 		{
 			JPanel buttonPane = new JPanel();
@@ -169,15 +174,24 @@ public class PersonSaveView extends JDialog {
 		
 	}
 	
+
+	private void cleanAllFields() {
+		
+		cleanFields();
+		tpResult.setText("");
+	}
+	
 	private void printResult(Optional<Person>optPerson) {
 		
 		
 		if (optPerson.isPresent()) {
 			
+			tpResult.setText(" = Person Saved =\n");
+			tpResult.setText( tpResult.getText() + optPerson.get().getPersonId() + " " + optPerson.get().getName() + " " + optPerson.get().getSurname());
 			
 		}else {
 			
-			
+			tpResult.setText(" = Empty =\n");
 		}
 		
 	}
