526ccd2dc2f1f3103795562adb631fdf845c6c35
IvanM <ivan.montes@outlook.com>
2023-12-18 20:09:44 +0100

    Add constructor and dependencies

--- src/main/java/ime/flixing/dao/impl/FlixPersonPositionDaoImpl.java
+++ src/main/java/ime/flixing/dao/impl/FlixPersonPositionDaoImpl.java
@@ -3,7 +3,7 @@ package ime.flixing.dao.impl;
 import java.util.List;
 
 import org.hibernate.Session;
-import org.hibernate.query.Query;
+import org.hibernate.SessionFactory;
 
 import ime.flixing.dao.FlixPersonPositionDao;
 import ime.flixing.entity.Flix;
@@ -15,56 +15,70 @@ import ime.flixing.util.HibernateUtil;
 
 public class FlixPersonPositionDaoImpl implements FlixPersonPositionDao {
 
+	private final SessionFactory sessionFactory;
+	
+	public FlixPersonPositionDaoImpl() {
+		super();
+		this.sessionFactory = HibernateUtil.getSession();
+	}
+
+	public FlixPersonPositionDaoImpl(SessionFactory sessionFactory) {
+		super();
+		this.sessionFactory = sessionFactory;
+	}
+
 	@Override
 	public FlixPersonPosition saveFlixPersonPosition(Long flixId, Long personId, Long positionId) {
 		
-		Session session = HibernateUtil.getSession().openSession();
-		session.beginTransaction();		
-		FlixPersonPositionId flixPersonPositionId = new FlixPersonPositionId(flixId, personId, positionId);		
-		Flix flixFound = session.get(Flix.class, flixId);
-		Person personFound = session.get(Person.class, personId);
-		Position positionFound = session.get(Position.class, positionId);
-		FlixPersonPosition flixPersonPosition = new FlixPersonPosition(flixPersonPositionId,flixFound,personFound,positionFound);
-		session.persist(flixPersonPosition);
-		session.getTransaction().commit();
-		FlixPersonPosition flixPersonPositionSaved = session.get(FlixPersonPosition.class, flixPersonPositionId);
-		session.close();
-		return flixPersonPositionSaved;
+		try(Session session = sessionFactory.openSession()){
+			
+			session.beginTransaction();		
+			FlixPersonPositionId flixPersonPositionId = new FlixPersonPositionId(flixId, personId, positionId);		
+			Flix flixFound = session.get(Flix.class, flixId);
+			Person personFound = session.get(Person.class, personId);
+			Position positionFound = session.get(Position.class, positionId);
+			FlixPersonPosition flixPersonPosition = new FlixPersonPosition(flixPersonPositionId,flixFound,personFound,positionFound);
+			session.persist(flixPersonPosition);
+			session.getTransaction().commit();
+			return session.get(FlixPersonPosition.class, flixPersonPositionId);
 		
-	}
+	}}
 
 	@Override
 	public void deleteFlixPersonPosition(Long flixId, Long personId, Long positionId) {
 		
-		Session session = HibernateUtil.getSession().openSession();
-		session.beginTransaction();		
-		FlixPersonPositionId flixPersonPositionId = new FlixPersonPositionId(flixId, personId, positionId);	
-		Flix flixFound = session.get(Flix.class, flixId);
-		Person personFound = session.get(Person.class, personId);
-		Position positionFound = session.get(Position.class, positionId);
-		FlixPersonPosition flixPersonPosition = new FlixPersonPosition(flixPersonPositionId,flixFound,personFound,positionFound);
-		session.remove(flixPersonPosition);
-		session.getTransaction().commit();
-		session.close();
+		try(Session session = sessionFactory.openSession()){
+			
+			session.beginTransaction();		
+			FlixPersonPositionId flixPersonPositionId = new FlixPersonPositionId(flixId, personId, positionId);	
+			Flix flixFound = session.get(Flix.class, flixId);
+			Person personFound = session.get(Person.class, personId);
+			Position positionFound = session.get(Position.class, positionId);
+			FlixPersonPosition flixPersonPosition = new FlixPersonPosition(flixPersonPositionId,flixFound,personFound,positionFound);
+			session.remove(flixPersonPosition);
+			session.getTransaction().commit();
 		
-	}
+	}}
 
 	@Override
 	public List<FlixPersonPosition> getAllFlixPersonPosition() {
-		Session session = HibernateUtil.getSession().openSession();
-		Query<FlixPersonPosition> query = session.createQuery("FROM FlixPersonPosition", FlixPersonPosition.class);
-		List<FlixPersonPosition> list = query.list();	
-		session.close();	
-		return list;
+		
+		try(Session session = sessionFactory.openSession()){
+			
+			return session.createQuery("FROM FlixPersonPosition", FlixPersonPosition.class).list();
+		
+		}
 	}
 
 	@Override
 	public FlixPersonPosition getFlixPersonPositionById(Long flixId, Long personId, Long positionId) {
-		Session session = HibernateUtil.getSession().openSession();
-		FlixPersonPositionId flixPersonPositionId = new FlixPersonPositionId(flixId, personId, positionId);
-		FlixPersonPosition flixPersonPosition = session.get(FlixPersonPosition.class, flixPersonPositionId);
-		session.close();
-		return flixPersonPosition;
+		
+		try(Session session = sessionFactory.openSession()){
+			
+			FlixPersonPositionId flixPersonPositionId = new FlixPersonPositionId(flixId, personId, positionId);
+			return session.get(FlixPersonPosition.class, flixPersonPositionId);
+	
+		}
 	}
 
 }
