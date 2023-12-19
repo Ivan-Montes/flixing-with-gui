66220ce2dd73e5678a4aaefb33b5850580e0e2be
IvanM <ivan.montes@outlook.com>
2023-12-19 13:10:25 +0100

    Replace specific DAO for GenericDao

--- src/main/java/ime/flixing/dao/impl/PositionDaoImpl.java
+++ src/main/java/ime/flixing/dao/impl/PositionDaoImpl.java
@@ -3,93 +3,90 @@ package ime.flixing.dao.impl;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
-import ime.flixing.dao.PositionDao;
+import ime.flixing.dao.GenericDao;
 import ime.flixing.entity.Position;
 import ime.flixing.util.HibernateUtil;
 
-public class PositionDaoImpl implements PositionDao{
+public class PositionDaoImpl implements GenericDao<Position>{
 
-	@Override
-	public List<Position> getAllPosition() {
-		
-		Session session = HibernateUtil.getSession().openSession();
-		Query<Position> query = session.createQuery("FROM Position", Position.class);
-		List<Position>list = query.list();
-		session.close();
-		return list;
-		
+	private final SessionFactory sessionFactory;	
+	
+	public PositionDaoImpl() {
+		super();
+		this.sessionFactory = HibernateUtil.getSession();
+	}
+
+	public PositionDaoImpl(SessionFactory sessionFactory) {
+		super();
+		this.sessionFactory = sessionFactory;
 	}
 
 	@Override
-	public Position getPositionById(Long id) {
-
-		Session session = HibernateUtil.getSession().openSession();
-		Position positionFound = session.get(Position.class, id);
-		session.close();
-		return positionFound;
-		
+	public List<Position> getAll() {
+		try( Session session = sessionFactory.openSession() ){
+			Query<Position> query = session.createQuery("FROM Position", Position.class);
+			return query.list();	
+		}
 	}
 
 	@Override
-	public Position savePosition(Position position) {
-
-		Session session = HibernateUtil.getSession().openSession();
-		session.beginTransaction();
-		session.persist(position);
-		session.getTransaction().commit();
-		Position positionFound = session.get(Position.class, position.getPositionId());
-		session.close();
-		return positionFound;
-		
+	public Position getById(Long id) {
+		try( Session session = sessionFactory.openSession() ){
+			return session.get(Position.class, id);
+		}
 	}
 
 	@Override
-	public Position updatePosition(Long id, Position position) {
-
-		Session session = HibernateUtil.getSession().openSession();
-		session.beginTransaction();
-		Position positioni = session.get(Position.class, id);
-		positioni.setName(position.getName());
-		positioni.setDescription(position.getDescription());
-		session.persist(positioni);
-		session.getTransaction().commit();
-		Position positionFound = session.get(Position.class, id);
-		session.close();
-		return positionFound;
-		
+	public Position getByIdEagger(Long id) {
+		try( Session session = sessionFactory.openSession() ){
+			Query<Position> query = session.createQuery("SELECT p FROM Position p LEFT JOIN FETCH p.flixPersonPosition WHERE p.positionId = :id", Position.class);
+			query.setParameter("id", id);
+			return query.uniqueResult();
+		}
 	}
 
 	@Override
-	public void deletePosition(Long id) {
-
-		Session session = HibernateUtil.getSession().openSession();
-		session.beginTransaction();
-		Position positionFound = session.get(Position.class, id);
-		session.remove(positionFound);
-		session.getTransaction().commit();
-        session.close();	
-		
+	public Position save(Position entity) {
+		try( Session session = sessionFactory.openSession() ){
+			session.beginTransaction();
+			session.persist(entity);
+			session.getTransaction().commit();
+			return session.get(Position.class, entity.getPositionId());
+		}
 	}
 
 	@Override
-	public Position getPositionByIdEagger(Long id) {
-		Session session = HibernateUtil.getSession().openSession();
-		Query<Position> query = session.createQuery("SELECT p FROM Position p LEFT JOIN FETCH p.flixPersonPosition WHERE p.positionId = :id", Position.class);
-		query.setParameter("id", id);
-		Position positionFound =  query.uniqueResult();
-		session.close();
-		return positionFound;
+	public Position update(Long id, Position entity) {
+		try( Session session = sessionFactory.openSession() ){
+			session.beginTransaction();
+			Position positioni = session.get(Position.class, id);
+			positioni.setName(entity.getName());
+			positioni.setDescription(entity.getDescription());
+			session.persist(positioni);
+			session.getTransaction().commit();
+			return session.get(Position.class, id);
+		}
 	}
 
 	@Override
-	public List<Position> getPositionByNameId(String name) {
-		Session session = HibernateUtil.getSession().openSession();
-		Query<Position> query = session.createQuery("FROM Position p WHERE p.name = :name", Position.class);
-		query.setParameter("name", name);
-		List<Position>list = query.list();
-		session.close();
-		return list;
+	public void delete(Long id) {
+		try( Session session = sessionFactory.openSession() ){
+			session.beginTransaction();
+			Position positionFound = session.get(Position.class, id);
+			session.remove(positionFound);
+			session.getTransaction().commit();
+		}
+	}
+
+	@Override
+	public List<Position> getByName(String name) {
+		try( Session session = sessionFactory.openSession() ){
+			Query<Position> query = session.createQuery("FROM Position p WHERE p.name = :name", Position.class);
+			query.setParameter("name", name);
+			return query.list();
+		}
 	}
 }
