aeae4566b6ab4ce1f79e74a3618d09089060504f
IvanM <ivan.montes@outlook.com>
2023-12-20 13:05:12 +0100

    Add logger and replace ActionListener for lambdas

--- src/main/java/ime/flixing/mvc/view/flix/FlixGetAllView.java
+++ src/main/java/ime/flixing/mvc/view/flix/FlixGetAllView.java
@@ -5,24 +5,24 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 import ime.flixing.entity.Flix;
 import ime.flixing.mvc.controller.FlixController;
+import ime.flixing.tool.DecoHelper;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.swing.SpringLayout;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
-import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import java.awt.Dimension;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 @Getter
 @Setter
@@ -41,10 +41,11 @@ public class FlixGetAllView extends JDialog {
 	public static void main(String[] args) {
 		try {
 			FlixGetAllView dialog = new FlixGetAllView();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 			dialog.setVisible(true);
 		} catch (Exception e) {
-			e.printStackTrace();
+			final Logger logger = Logger.getLogger(FlixGetAllView.class.getName());
+			logger.log(Level.SEVERE, DecoHelper.MSG_SHIT_HAPPENS, e);
 		}
 	}
 
@@ -52,71 +53,48 @@ public class FlixGetAllView extends JDialog {
 	 * Create the dialog.
 	 */
 	public FlixGetAllView() {
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 		setModal(true);
 		setAlwaysOnTop(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		SpringLayout sl_contentPanel = new SpringLayout();
-		contentPanel.setLayout(sl_contentPanel);
+		SpringLayout slContentPanel = new SpringLayout();
+		contentPanel.setLayout(slContentPanel);
 		
 		btnSearch = new JButton("Search");
-		btnSearch.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				searchAndShowFlixes();
-				
-			}
-		});
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, btnSearch, 10, SpringLayout.NORTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, btnSearch, 159, SpringLayout.WEST, contentPanel);
+		btnSearch.addActionListener( e -> searchAndShowFlixes() );				
+		slContentPanel.putConstraint(SpringLayout.NORTH, btnSearch, 10, SpringLayout.NORTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.WEST, btnSearch, 159, SpringLayout.WEST, contentPanel);
 		contentPanel.add(btnSearch);
 		
 		btnBack = new JButton("Back");
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnBack, -10, SpringLayout.SOUTH, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, btnBack, 0, SpringLayout.EAST, btnSearch);
-		btnBack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				dispose();
-				
-			}
-		});
+		slContentPanel.putConstraint(SpringLayout.SOUTH, btnBack, -10, SpringLayout.SOUTH, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.EAST, btnBack, 0, SpringLayout.EAST, btnSearch);
+		btnBack.addActionListener( e -> dispose());
 		contentPanel.add(btnBack);
 		
 		spFlixes = new JScrollPane();
-		sl_contentPanel.putConstraint(SpringLayout.NORTH, spFlixes, 12, SpringLayout.SOUTH, btnSearch);
-		sl_contentPanel.putConstraint(SpringLayout.WEST, spFlixes, 5, SpringLayout.WEST, contentPanel);
-		sl_contentPanel.putConstraint(SpringLayout.SOUTH, spFlixes, -6, SpringLayout.NORTH, btnBack);
-		sl_contentPanel.putConstraint(SpringLayout.EAST, spFlixes, 419, SpringLayout.WEST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.NORTH, spFlixes, 12, SpringLayout.SOUTH, btnSearch);
+		slContentPanel.putConstraint(SpringLayout.WEST, spFlixes, 5, SpringLayout.WEST, contentPanel);
+		slContentPanel.putConstraint(SpringLayout.SOUTH, spFlixes, -6, SpringLayout.NORTH, btnBack);
+		slContentPanel.putConstraint(SpringLayout.EAST, spFlixes, 419, SpringLayout.WEST, contentPanel);
 		spFlixes.setPreferredSize(new Dimension(73, 39));
 		contentPanel.add(spFlixes);
 		
 		jTableFlixes = new JTable();
 		jTableFlixes.setFillsViewportHeight(true);
-		jTableFlixes.setModel(new javax.swing.table.DefaultTableModel(
-	            new Object [][] {
-	                {null, null, null, null}
-	            },
-	            new String [] {
-	            		"FlixId", "Title", "GenreId", "GenreName"
-	            }
-	        ));
-
-		jTableFlixes.setName("jTableFlixes");
 		spFlixes.setViewportView(jTableFlixes);
 		
 	}
 	
 	private void searchAndShowFlixes() {		
 
-		Optional<List<Flix>> optListFlix = FlixController.searchAll();
+		Optional<List<Flix>> optListFlix = new FlixController().getAllFlix();
 		
 		if ( optListFlix.isPresent() ) {			
 			
-			String [] aTitles = {"FlixId", "Title", "GenreId", "GenreName"};
 			Object[][] aFlix = IntStream.range(0, optListFlix.get().size() )
                     .mapToObj( i -> new Object []{
                     		optListFlix.get().get(i).getFlixId(),
@@ -126,10 +104,30 @@ public class FlixGetAllView extends JDialog {
                     		})
                     .toArray(Object[][]::new);
 			
-			DefaultTableModel modelTabla = new DefaultTableModel(aFlix,aTitles);
+			DefaultTableModel modelTabla = new DefaultTableModel(aFlix, new String []{"FlixId", "Title", "GenreId", "GenreName"}){
+				
+		        /**
+				 * JTable treats the first columns as Object instead of Integer. Now, it should sort based on the integer values.
+				 */
+				private static final long serialVersionUID = 6529289790518969819L;
+
+				@Override
+		        public Class<?> getColumnClass(int columnIndex) {
+		            if (columnIndex == 0) return Integer.class;
+		            return super.getColumnClass(columnIndex);
+		        }
+				
+				/**
+				 * JTable treats cells as not editable
+				 */
+				@Override
+			    public boolean isCellEditable(int row, int column) {			       
+			       return false;
+			    }
+		    };
+		    
 		    jTableFlixes.setModel(modelTabla);
-		    TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(jTableFlixes.getModel());
-		    jTableFlixes.setRowSorter(sorter);
+		    jTableFlixes.setRowSorter(new TableRowSorter<>(jTableFlixes.getModel()));
 		    jTableFlixes.getColumnModel().getColumn(0).setPreferredWidth(22);
 		    jTableFlixes.getColumnModel().getColumn(2).setPreferredWidth(22);
 		    jTableFlixes.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
