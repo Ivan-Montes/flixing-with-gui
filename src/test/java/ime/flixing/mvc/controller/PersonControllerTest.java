7fc14dba1ab883ac2c5a094ee97ea9bfa03babc8
IvanM <ivan.montes@outlook.com>
2024-01-01 18:57:57 +0100

    Adapt methods to new GenericDao

--- src/test/java/ime/flixing/mvc/controller/PersonControllerTest.java
+++ src/test/java/ime/flixing/mvc/controller/PersonControllerTest.java
@@ -144,7 +144,7 @@ class PersonControllerTest {
 	void PersonController_getAllPerson_ReturnOptListPerson() {
 		
 		personList.add(personTest);
-		doReturn(personList).when(personDaoImpl).getAllPerson();
+		doReturn(personList).when(personDaoImpl).getAll();
 		
 		Optional<List<Person>>optListPerson = personController.getAllPerson();
 		
@@ -154,7 +154,7 @@ class PersonControllerTest {
 				()->Assertions.assertThat(optListPerson.get().get(0).getPersonId()).isEqualTo(personTestId),
 				()->Assertions.assertThat(optListPerson.get().get(0).getName()).isEqualTo(personTestName)
 				);
-		verify(personDaoImpl,times(1)).getAllPerson();		
+		verify(personDaoImpl,times(1)).getAll();		
 		
 	}
 	
@@ -164,7 +164,7 @@ class PersonControllerTest {
 		try( MockedStatic<Checker>mockChecker = Mockito.mockStatic(Checker.class) ){
 			
 			mockChecker.when( () -> Checker.checkDigits(Mockito.anyString()) ).thenReturn(true);
-			doReturn(personTest).when(personDaoImpl).getPersonById(Mockito.anyLong());
+			doReturn(personTest).when(personDaoImpl).getById(Mockito.anyLong());
 			
 			Optional<Person>optPerson = personController.getPersonById(personTestIdStr);
 			
@@ -173,7 +173,7 @@ class PersonControllerTest {
 					()->Assertions.assertThat(optPerson.get().getPersonId()).isEqualTo(personTestId),
 					()->Assertions.assertThat(optPerson.get().getName()).isEqualTo(personTestName)
 					);
-			verify(personDaoImpl,times(1)).getPersonById(Mockito.anyLong());
+			verify(personDaoImpl,times(1)).getById(Mockito.anyLong());
 			mockChecker.verify(() -> Checker.checkDigits(Mockito.anyString()), times(1));
 		}		
 	}
@@ -186,7 +186,7 @@ class PersonControllerTest {
 			
 			mockChecker.when( () -> Checker.checkName(Mockito.anyString()) ).thenReturn(true);
 			mockChecker.when( () -> Checker.checkSurname(Mockito.anyString()) ).thenReturn(true);
-			doReturn(personTest).when(personDaoImpl).savePerson(Mockito.any(Person.class));
+			doReturn(personTest).when(personDaoImpl).save(Mockito.any(Person.class));
 			
 			Optional<Person>optPerson = personController.savePerson("","");
 			
@@ -196,7 +196,7 @@ class PersonControllerTest {
 					()->Assertions.assertThat(optPerson.get().getName()).isEqualTo(personTestName),
 					()->Assertions.assertThat(optPerson.get().getSurname()).isEqualTo(personTestSurname)
 					);
-			verify(personDaoImpl,times(1)).savePerson(Mockito.any(Person.class));
+			verify(personDaoImpl,times(1)).save(Mockito.any(Person.class));
 			mockChecker.verify(() -> Checker.checkName(Mockito.anyString()), times(1));			
 			mockChecker.verify(() -> Checker.checkSurname(Mockito.anyString()), times(1));			
 			}		
@@ -209,8 +209,8 @@ class PersonControllerTest {
 			mockChecker.when( () -> Checker.checkDigits(Mockito.anyString()) ).thenReturn(true);
 			mockChecker.when( () -> Checker.checkName(Mockito.anyString()) ).thenReturn(true);
 			mockChecker.when( () -> Checker.checkSurname(Mockito.anyString()) ).thenReturn(true);
-			doReturn(personTest).when(personDaoImpl).getPersonById(Mockito.anyLong());
-			doReturn(personTest).when(personDaoImpl).updatePerson(Mockito.anyLong(), Mockito.any(Person.class));
+			doReturn(personTest).when(personDaoImpl).getById(Mockito.anyLong());
+			doReturn(personTest).when(personDaoImpl).update(Mockito.anyLong(), Mockito.any(Person.class));
 			
 			Optional<Person>optPerson = personController.updatePerson(personTestIdStr, "", "");
 			
@@ -220,8 +220,8 @@ class PersonControllerTest {
 					()->Assertions.assertThat(optPerson.get().getName()).isEqualTo(personTestName),
 					()->Assertions.assertThat(optPerson.get().getSurname()).isEqualTo(personTestSurname)
 					);
-			verify(personDaoImpl,times(1)).getPersonById(Mockito.anyLong());
-			verify(personDaoImpl,times(1)).updatePerson(Mockito.anyLong(), Mockito.any(Person.class));
+			verify(personDaoImpl,times(1)).getById(Mockito.anyLong());
+			verify(personDaoImpl,times(1)).update(Mockito.anyLong(), Mockito.any(Person.class));
 			mockChecker.verify(() -> Checker.checkDigits(Mockito.anyString()), times(1));
 			mockChecker.verify(() -> Checker.checkName(Mockito.anyString()), times(1));			
 			mockChecker.verify(() -> Checker.checkSurname(Mockito.anyString()), times(1));		
@@ -234,16 +234,16 @@ class PersonControllerTest {
 		try( MockedStatic<Checker>mockChecker = Mockito.mockStatic(Checker.class) ){
 			
 			mockChecker.when( () -> Checker.checkDigits(Mockito.anyString()) ).thenReturn(true);
-			doReturn(personTest).when(personDaoImpl).getPersonByIdEagger(Mockito.anyLong());
-			doNothing().when(personDaoImpl).deletePerson(Mockito.anyLong());			
+			doReturn(personTest).when(personDaoImpl).getByIdEagger(Mockito.anyLong());
+			doNothing().when(personDaoImpl).delete(Mockito.anyLong());			
 
 			int returnValue = personController.deletePerson(personTestIdStr);
 			
 			assertAll(
 					()->Assertions.assertThat(returnValue).isZero()
 					);
-			verify(personDaoImpl,times(1)).getPersonByIdEagger(Mockito.anyLong());
-			verify(personDaoImpl,times(1)).deletePerson(Mockito.anyLong());
+			verify(personDaoImpl,times(1)).getByIdEagger(Mockito.anyLong());
+			verify(personDaoImpl,times(1)).delete(Mockito.anyLong());
 			mockChecker.verify(() -> Checker.checkDigits(Mockito.anyString()), times(1));
 		}	
 	}
@@ -255,14 +255,14 @@ class PersonControllerTest {
 			
 			mockChecker.when( () -> Checker.checkDigits(Mockito.anyString()) ).thenReturn(true);
 			personTest.getFlixPersonPosition().add(new FlixPersonPosition());
-			doReturn(personTest).when(personDaoImpl).getPersonByIdEagger(Mockito.anyLong());
+			doReturn(personTest).when(personDaoImpl).getByIdEagger(Mockito.anyLong());
 
 			int returnValue = personController.deletePerson(personTestIdStr);
 			
 			assertAll(
 					()->Assertions.assertThat(returnValue).isEqualTo(-2L)
 					);
-			verify(personDaoImpl,times(1)).getPersonByIdEagger(Mockito.anyLong());
+			verify(personDaoImpl,times(1)).getByIdEagger(Mockito.anyLong());
 			mockChecker.verify(() -> Checker.checkDigits(Mockito.anyString()), times(1));
 		}	
 	}
