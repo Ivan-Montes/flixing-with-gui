f6ac44a0d5cfb078388a2dbd042f2ce5e6497c14
IvanM <ivan.montes@outlook.com>
2024-01-08 18:07:36 +0100

    Reorganize main class

--- src/main/java/ime/flixing/App.java
+++ src/main/java/ime/flixing/App.java
@@ -8,14 +8,34 @@ import ime.flixing.util.HibernateUtil;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
     
-    	Thread.setDefaultUncaughtExceptionHandler(new TheUncaughtExceptionHandler());
-
-    	HibernateUtil.getSession();
-    	
-    	new MainController().init();
+    	App app = new App();   
+    	app.init();    	 	
     	
     }
+    
+    void init() {
+    	
+    	initExceptionHandler();
+    	initHibernate();
+    	initGui();
+    	
+    }
+    
+     void initExceptionHandler() {
+    	
+    	Thread.setDefaultUncaughtExceptionHandler(new TheUncaughtExceptionHandler());
+    }
+
+     void initHibernate() {
+    	
+    	HibernateUtil.getSession();    	
+    }
+
+     void initGui() {    	
+    	
+    	new MainController().init();
+    }
+    
 }
