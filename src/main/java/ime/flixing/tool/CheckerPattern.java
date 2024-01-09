ee9555dc2e19443dfe8c75b5f0b216c550ec8f0f
IvanM <ivan.montes@outlook.com>
2024-01-09 19:28:06 +0100

    Add more characters to DESCRITPION constants

--- src/main/java/ime/flixing/tool/CheckerPattern.java
+++ src/main/java/ime/flixing/tool/CheckerPattern.java
@@ -13,8 +13,8 @@ public class CheckerPattern {
 	public static final String TITLE_FLIX_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:]{1,50}";
 	public static final String NAME_BASIC = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+";
 	public static final String NAME_FULL = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{1,50}";
-	public static final String DESCRIPTION_BASIC = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+";
-	public static final String DESCRIPTION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{1,100}";
+	public static final String DESCRIPTION_BASIC = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:]+";
+	public static final String DESCRIPTION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:]{1,100}";
 	public static final String SURNAME_BASIC = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+";
 	public static final String SURNAME_FULL = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{1,50}";
 
