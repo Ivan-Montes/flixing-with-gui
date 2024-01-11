29301040b9596c88675cfeedced8dbf83900c8a9
IvanM <ivan.montes@outlook.com>
2024-01-11 19:17:00 +0100

    Delete unused setting in StandardServiceRegistryBuilder

--- src/main/java/ime/flixing/util/HibernateUtil.java
+++ src/main/java/ime/flixing/util/HibernateUtil.java
@@ -24,7 +24,6 @@ public class HibernateUtil {
 																	.applySetting("hibernate.connection.url", "jdbc:mysql://localhost:3306/flixingdb?useSSL=false")
 																	.applySetting("hibernate.connection.username", System.getenv("mysql.flixingdb.user"))
 													                .applySetting("hibernate.connection.password", System.getenv("mysql.flixingdb.password"))
-													                //.applySetting("hibernate.show_sql", "true")
 													                .applySetting("hibernate.hbm2ddl.auto", "create")
 													                .applySetting("hibernate.hbm2ddl.import_files", "data.sql")
 																	.configure()
@@ -44,7 +43,7 @@ public class HibernateUtil {
 			
 			} catch (RuntimeException  ex) {
 				StandardServiceRegistryBuilder.destroy( registry );
-				throw new ExceptionInInitializerError(ex + " ##==== Inicio de SessionFactory erroneo => RuntimeException ====##\n ");
+				throw new ExceptionInInitializerError(ex + " ##==== Inicio de SessionFactory erroneo => RuntimeException ====##\n ");				
 		}
 	}
 
