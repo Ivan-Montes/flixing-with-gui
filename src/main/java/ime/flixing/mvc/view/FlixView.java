030ebe6dbc45406edce1f5007b3820daee489a0f
IvanM <ivan.montes@outlook.com>
2024-01-14 16:48:12 +0100

    Add logger instead of printing exception.

--- src/main/java/ime/flixing/mvc/view/FlixView.java
+++ src/main/java/ime/flixing/mvc/view/FlixView.java
@@ -2,17 +2,18 @@ package ime.flixing.mvc.view;
 
 
 import javax.swing.JDialog;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import ime.flixing.mvc.controller.FlixController;
+import ime.flixing.tool.DecoHelper;
 
 import javax.swing.SpringLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class FlixView extends JDialog {
 
@@ -25,10 +26,11 @@ public class FlixView extends JDialog {
 	public static void main(String[] args) {
 					try {
 					FlixView frame = new FlixView();
-					frame.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					frame.setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
 					frame.setVisible(true);
 				} catch (Exception e) {
-					e.printStackTrace();
+					final Logger logger = Logger.getLogger(FlixView.class.getName());
+					logger.log(Level.SEVERE, DecoHelper.MSG_SHIT_HAPPENS, e);
 				}
 			
 		
@@ -45,84 +47,49 @@ public class FlixView extends JDialog {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		SpringLayout sl_contentPane = new SpringLayout();
-		contentPane.setLayout(sl_contentPane);
+		SpringLayout slContentPane = new SpringLayout();
+		contentPane.setLayout(slContentPane);
 		
 		JLabel lblFlixTitle = new JLabel("FLIX");
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblFlixTitle, 166, SpringLayout.WEST, contentPane);
+		slContentPane.putConstraint(SpringLayout.WEST, lblFlixTitle, 168, SpringLayout.WEST, contentPane);
 		lblFlixTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
 		contentPane.add(lblFlixTitle);
 		
-		JButton btnGetAllFlix = new JButton("getAllFlix");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnGetAllFlix, 33, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblFlixTitle, -6, SpringLayout.NORTH, btnGetAllFlix);
-		btnGetAllFlix.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				FlixController.initFlixGetAllView();
-				
-			}
-		});
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnGetAllFlix, 142, SpringLayout.WEST, contentPane);
+		JButton btnGetAllFlix = new JButton("Get All");
+		slContentPane.putConstraint(SpringLayout.SOUTH, lblFlixTitle, -13, SpringLayout.NORTH, btnGetAllFlix);
+		btnGetAllFlix.addActionListener( e -> FlixController.initFlixGetAllView());
 		contentPane.add(btnGetAllFlix);
 		
-		JButton btnGetFlixById = new JButton("getFlixById");
-		btnGetFlixById.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				FlixController.initFlixGetByIdView();
-			}
-		});
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnGetFlixById, 6, SpringLayout.SOUTH, btnGetAllFlix);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnGetFlixById, 0, SpringLayout.WEST, btnGetAllFlix);
+		JButton btnGetFlixById = new JButton("Get By Id");
+		slContentPane.putConstraint(SpringLayout.WEST, btnGetFlixById, 156, SpringLayout.WEST, contentPane);
+		slContentPane.putConstraint(SpringLayout.WEST, btnGetAllFlix, 0, SpringLayout.WEST, btnGetFlixById);
+		slContentPane.putConstraint(SpringLayout.SOUTH, btnGetAllFlix, -6, SpringLayout.NORTH, btnGetFlixById);
+		btnGetFlixById.addActionListener( e -> FlixController.initFlixGetByIdView() );
 		contentPane.add(btnGetFlixById);
 		
-		JButton btnSaveFlix = new JButton("saveFlix");
-		btnSaveFlix.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				FlixController.initFlixSaveView();
-				
-			}
-		});
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnSaveFlix, 6, SpringLayout.SOUTH, btnGetFlixById);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnSaveFlix, 0, SpringLayout.WEST, btnGetAllFlix);
+		JButton btnSaveFlix = new JButton("Save");
+		slContentPane.putConstraint(SpringLayout.WEST, btnSaveFlix, 156, SpringLayout.WEST, contentPane);
+		slContentPane.putConstraint(SpringLayout.SOUTH, btnGetFlixById, -6, SpringLayout.NORTH, btnSaveFlix);
+		btnSaveFlix.addActionListener( e -> FlixController.initFlixSaveView());
 		contentPane.add(btnSaveFlix);
 		
-		JButton btnUpdateflix = new JButton("updateFlix");
-		btnUpdateflix.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				FlixController.initFlixUpdateView();
-				
-			}
-		});
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnUpdateflix, 6, SpringLayout.SOUTH, btnSaveFlix);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnUpdateflix, 0, SpringLayout.WEST, btnGetAllFlix);
+		JButton btnUpdateflix = new JButton("Update");
+		slContentPane.putConstraint(SpringLayout.WEST, btnUpdateflix, 156, SpringLayout.WEST, contentPane);
+		slContentPane.putConstraint(SpringLayout.SOUTH, btnSaveFlix, -6, SpringLayout.NORTH, btnUpdateflix);
+		btnUpdateflix.addActionListener( e -> FlixController.initFlixUpdateView());
 		contentPane.add(btnUpdateflix);
 		
-		JButton btnDeleteFlix = new JButton("deleteFlix");
-		btnDeleteFlix.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				FlixController.initFlixDeleteView();
-				
-			}
-		});
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnDeleteFlix, 6, SpringLayout.SOUTH, btnUpdateflix);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnDeleteFlix, 0, SpringLayout.WEST, btnGetAllFlix);
+		JButton btnDeleteFlix = new JButton("Delete");
+		slContentPane.putConstraint(SpringLayout.NORTH, btnDeleteFlix, 156, SpringLayout.NORTH, contentPane);
+		slContentPane.putConstraint(SpringLayout.WEST, btnDeleteFlix, 156, SpringLayout.WEST, contentPane);
+		slContentPane.putConstraint(SpringLayout.SOUTH, btnUpdateflix, -6, SpringLayout.NORTH, btnDeleteFlix);
+		btnDeleteFlix.addActionListener( e -> FlixController.initFlixDeleteView());
 		contentPane.add(btnDeleteFlix);
 		
 		JButton btnBack = new JButton("Back");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnBack, -10, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnBack, -10, SpringLayout.EAST, contentPane);
-		btnBack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				dispose();
-				
-			}
-		});
+		slContentPane.putConstraint(SpringLayout.SOUTH, btnBack, -10, SpringLayout.SOUTH, contentPane);
+		slContentPane.putConstraint(SpringLayout.EAST, btnBack, -10, SpringLayout.EAST, contentPane);
+		btnBack.addActionListener( e -> dispose());
 		contentPane.add(btnBack);
 	}
 }
