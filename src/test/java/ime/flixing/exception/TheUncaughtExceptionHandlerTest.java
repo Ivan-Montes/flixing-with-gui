e58cf6f1ce65e5132b86cb1e8569d1e9d29c0f98
IvanM <ivan.montes@outlook.com>
2024-01-17 12:06:08 +0100

    Add mocks and inject dependencies in TheUncaughtExceptionHandler test

--- src/test/java/ime/flixing/exception/TheUncaughtExceptionHandlerTest.java
+++ src/test/java/ime/flixing/exception/TheUncaughtExceptionHandlerTest.java
@@ -11,15 +11,29 @@ import java.util.logging.Logger;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ime.flixing.tool.MsgBox;
 
 
 @ExtendWith(MockitoExtension.class)
 class TheUncaughtExceptionHandlerTest {
 	
+	@InjectMocks
+	private TheUncaughtExceptionHandler miniChefHandler;
+	
+	@Mock
+	private MsgBox msgBoxM;
+	
+	@Mock
+	private Logger loggerM;
+	
+	
 	private Thread thread;	
+	
 	
 	@Test
 	void UncaughtExceptionHandler_Null_ReturnVoid() {
@@ -78,14 +92,32 @@ class TheUncaughtExceptionHandlerTest {
 	@Test
 	void UncaughtExceptionHandler_ServiceException_ReturnVoid() {
 		
-		try(MockedStatic<Logger>mockLogger = Mockito.mockStatic(Logger.class)){
+		doNothing().when(loggerM).log(Mockito.any(Level.class), Mockito.anyString());
+		thread = new Thread( () -> { throw new org.hibernate.service.spi.ServiceException("How exception"); } );
+		thread.setUncaughtExceptionHandler(miniChefHandler);
+		
+		thread.start();
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
 			
-			Logger loggerFake = Mockito.mock(Logger.class);
-			mockLogger.when( () -> Logger.getLogger(Mockito.anyString())).thenReturn(loggerFake);
-			doNothing().when(loggerFake).log(Mockito.any(Level.class), Mockito.anyString());
-			thread = new Thread( () -> { throw new org.hibernate.service.spi.ServiceException("How exception"); } );
-			TheUncaughtExceptionHandler miniHandler = new TheUncaughtExceptionHandler();
-			thread.setUncaughtExceptionHandler(miniHandler);
+		}
+		
+			assertAll(
+					()->Assertions.assertThat(thread).isNotNull()
+					);				
+		verify(loggerM, times(1)).log(Mockito.any(Level.class), Mockito.anyString());
+			
+	}	
+
+	
+	@Test
+	void UncaughtExceptionHandler_ConstraintViolation_ReturnVoid() {
+			
+			doNothing().when(loggerM).log(Mockito.any(Level.class), Mockito.anyString());			
+			doNothing().when(msgBoxM).showExeptionDialog(Mockito.any());			
+			thread = new Thread( () -> { throw new jakarta.validation.ConstraintViolationException(null); } );
+			thread.setUncaughtExceptionHandler(miniChefHandler);
 			
 			thread.start();
 			try {
@@ -97,36 +129,30 @@ class TheUncaughtExceptionHandlerTest {
 				assertAll(
 						()->Assertions.assertThat(thread).isNotNull()
 						);				
-			verify(loggerFake, times(1)).log(Mockito.any(Level.class), Mockito.anyString());
-				
-		}
-	}
+			verify(loggerM, times(1)).log(Mockito.any(Level.class), Mockito.anyString());
+			verify(msgBoxM, times(1)).showExeptionDialog(Mockito.any());		
+	}	
+	
 
 	@Test
 	void UncaughtExceptionHandler_Others_ReturnVoid() {
 
-		try(MockedStatic<Logger>mockLogger = Mockito.mockStatic(Logger.class)){
+		doNothing().when(loggerM).log(Mockito.any(Level.class), Mockito.anyString());			
+		thread = new Thread( () -> { throw new RuntimeException(); } );
+		thread.setUncaughtExceptionHandler(miniChefHandler);
+		
+		thread.start();
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
 			
-			Logger loggerFake = Mockito.mock(Logger.class);
-			mockLogger.when( () -> Logger.getLogger(Mockito.anyString())).thenReturn(loggerFake);
-			doNothing().when(loggerFake).log(Mockito.any(Level.class), Mockito.anyString());
-			thread = new Thread( () -> { throw new RuntimeException(); } );
-			TheUncaughtExceptionHandler miniHandler = new TheUncaughtExceptionHandler();
-			thread.setUncaughtExceptionHandler(miniHandler);
-			
-			thread.start();
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				
-			}
-			
-				assertAll(
-						()->Assertions.assertThat(thread).isNotNull()
-						);				
-			verify(loggerFake, times(1)).log(Mockito.any(Level.class), Mockito.anyString());
-				
 		}
+		
+			assertAll(
+					()->Assertions.assertThat(thread).isNotNull()
+					);				
+		verify(loggerM, times(1)).log(Mockito.any(Level.class), Mockito.anyString());
+				
 	}	
 	
 	
